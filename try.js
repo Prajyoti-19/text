d0f07892059bd74184fc156f30c4d566e08fc736
Prajyoti Gaikwad <92573827+Prajyoti-19@users.noreply.github.com>
2023-11-22 00:43:40 +0530

    Add files via upload

---
+++ try.js
@@ -0,0 +1,5 @@
+function learn(){
+    let x = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet eius dolore illum  sit amet consectetur adipisicing elit. Amet eius dolore illum sit amet consectetur adipisicing elit. Amet eius dolore illum assumenda labore, cupiditate nobis vitae autem";
+    document.getElementById("demo").innerHTML = x;
+}
+
